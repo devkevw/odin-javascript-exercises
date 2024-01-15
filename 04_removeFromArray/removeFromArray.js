7440074a89b1d8c9a7a57009e864f6c969a4e2b1
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-14 20:06:18 -0500

    Add solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    let argsArray = Array.from(arguments)
+    const arr = argsArray[0];
+    const elmts = argsArray.slice(1);
+    let newArr = [];
+    
+    for (const e of arr) {
+        if (!(elmts.includes(e))) newArr.push(e);
+    }
 
+    return newArr;
 };
 
 // Do not edit below this line
