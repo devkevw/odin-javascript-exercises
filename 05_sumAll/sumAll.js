cf90a5dce54e21fe27836fc119b902a870063d26
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-14 20:16:42 -0500

    Add solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    // input validation
+    if (typeof n != "number" || typeof m !='number') return 'ERROR';  
+    if (n < 0 || m < 0 ) return 'ERROR';
 
+    if (n > m) {  // swap variables
+        let temp = n;
+        n = m;
+        m = temp;
+    }
+    
+    let sum = 0
+    for (let i = n; i <= m; i++) {
+        sum += i;
+    }
+
+    return sum; 
 };
 
 // Do not edit below this line
