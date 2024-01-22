c04b2f497a8062d669e7764b70b28317d50adb39
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-22 12:36:26 -0500

    Add solution to fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0) return 0;
+    if (num < 0) return 'OOPS';
+    
+    let first = 1;
+    let second = 1; 
 
+    for (let i = 1; i < num; i++){
+        let temp = first;
+        first = second; 
+        second = first + temp;
+    }
+
+    return first;
 };
 
 // Do not edit below this line
