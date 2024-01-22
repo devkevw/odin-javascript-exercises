f8d8d2c11b18844853387385eaa52b43a4cb417a
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-22 12:19:48 -0500

    Add solution to palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g;
 
+
+const palindromes = function (word) {
+    let words = word.split(punctuationRegex);
+    let cleaned = words.join('').toLowerCase();
+    console.log(cleaned)
+    let reverse = cleaned.split('').reverse().join("");
+    return reverse == cleaned;
 };
 
 // Do not edit below this line
