2e42b474607658af12de97673e227da9bd18e483
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-22 19:18:29 -0500

    Add solution to findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    return people.sort(function (a, b) { 
+        if (!('yearOfDeath' in a )){a.yearOfDeath = 2024};
+        if (!('yearOfDeath' in b )){b.yearOfDeath = 2024};
 
+        return (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) ? 1: -1;
+    
+    })[0];
 };
 
 // Do not edit below this line
